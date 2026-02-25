fce798f2111fd1e51963fd3b3bdff9acea4fefaa
hong4137 <hong4137@paran.com>
2026-02-26 00:07:33 +0900

    PWA: register service worker

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
+import { registerServiceWorker } from "./lib/push";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// Register service worker for PWA + push
+registerServiceWorker();
